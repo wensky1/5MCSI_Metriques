039bc101a5dea46a7c75a274fef8894826fc325d
wensky1 <wgarreau@myges.fr>
2024-09-12 17:01:46 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -35,6 +35,9 @@ def mongraphique():
 def monhistogramme():
     return render_template("histogramme.html")
 
+@app.route("/contact/")
+def moncontact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
