c1bffd330a5efd710d3a7834508710f74de46429
wensky1 <wgarreau@myges.fr>
2024-09-12 15:58:03 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,10 +6,13 @@ from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+ @app.route("/contact/")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>"                                                                                                                                      
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #comm2
   
 if __name__ == "__main__":
   app.run(debug=True)
+
